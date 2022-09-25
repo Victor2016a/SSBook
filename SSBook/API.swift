b978c47ecef0c53dfbf590258dcfeb1311351488
Victor <victor.a.vieira@hotmail.com>
2022-09-25 10:43:00 -0400

    Making 5 queries separated

--- SSBook/API.swift
+++ SSBook/API.swift
@@ -4,11 +4,269 @@
 import Apollo
 import Foundation
 
-public final class DetailsViewQuery: GraphQLQuery {
+public final class AllBooksQuery: GraphQLQuery {
   /// The raw GraphQL definition of this operation.
   public let operationDefinition: String =
     """
-    query DetailsView($id: ID!) {
+    query AllBooks {
+      allBooks {
+        __typename
+        cover
+        name
+        author {
+          __typename
+          name
+        }
+      }
+    }
+    """
+
+  public let operationName: String = "AllBooks"
+
+  public init() {
+  }
+
+  public struct Data: GraphQLSelectionSet {
+    public static let possibleTypes: [String] = ["Query"]
+
+    public static var selections: [GraphQLSelection] {
+      return [
+        GraphQLField("allBooks", type: .nonNull(.list(.nonNull(.object(AllBook.selections))))),
+      ]
+    }
+
+    public private(set) var resultMap: ResultMap
+
+    public init(unsafeResultMap: ResultMap) {
+      self.resultMap = unsafeResultMap
+    }
+
+    public init(allBooks: [AllBook]) {
+      self.init(unsafeResultMap: ["__typename": "Query", "allBooks": allBooks.map { (value: AllBook) -> ResultMap in value.resultMap }])
+    }
+
+    public var allBooks: [AllBook] {
+      get {
+        return (resultMap["allBooks"] as! [ResultMap]).map { (value: ResultMap) -> AllBook in AllBook(unsafeResultMap: value) }
+      }
+      set {
+        resultMap.updateValue(newValue.map { (value: AllBook) -> ResultMap in value.resultMap }, forKey: "allBooks")
+      }
+    }
+
+    public struct AllBook: GraphQLSelectionSet {
+      public static let possibleTypes: [String] = ["Book"]
+
+      public static var selections: [GraphQLSelection] {
+        return [
+          GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+          GraphQLField("cover", type: .nonNull(.scalar(String.self))),
+          GraphQLField("name", type: .nonNull(.scalar(String.self))),
+          GraphQLField("author", type: .nonNull(.object(Author.selections))),
+        ]
+      }
+
+      public private(set) var resultMap: ResultMap
+
+      public init(unsafeResultMap: ResultMap) {
+        self.resultMap = unsafeResultMap
+      }
+
+      public init(cover: String, name: String, author: Author) {
+        self.init(unsafeResultMap: ["__typename": "Book", "cover": cover, "name": name, "author": author.resultMap])
+      }
+
+      public var __typename: String {
+        get {
+          return resultMap["__typename"]! as! String
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "__typename")
+        }
+      }
+
+      public var cover: String {
+        get {
+          return resultMap["cover"]! as! String
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "cover")
+        }
+      }
+
+      public var name: String {
+        get {
+          return resultMap["name"]! as! String
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "name")
+        }
+      }
+
+      public var author: Author {
+        get {
+          return Author(unsafeResultMap: resultMap["author"]! as! ResultMap)
+        }
+        set {
+          resultMap.updateValue(newValue.resultMap, forKey: "author")
+        }
+      }
+
+      public struct Author: GraphQLSelectionSet {
+        public static let possibleTypes: [String] = ["Author"]
+
+        public static var selections: [GraphQLSelection] {
+          return [
+            GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+            GraphQLField("name", type: .nonNull(.scalar(String.self))),
+          ]
+        }
+
+        public private(set) var resultMap: ResultMap
+
+        public init(unsafeResultMap: ResultMap) {
+          self.resultMap = unsafeResultMap
+        }
+
+        public init(name: String) {
+          self.init(unsafeResultMap: ["__typename": "Author", "name": name])
+        }
+
+        public var __typename: String {
+          get {
+            return resultMap["__typename"]! as! String
+          }
+          set {
+            resultMap.updateValue(newValue, forKey: "__typename")
+          }
+        }
+
+        public var name: String {
+          get {
+            return resultMap["name"]! as! String
+          }
+          set {
+            resultMap.updateValue(newValue, forKey: "name")
+          }
+        }
+      }
+    }
+  }
+}
+
+public final class FavoriteAuthorsQuery: GraphQLQuery {
+  /// The raw GraphQL definition of this operation.
+  public let operationDefinition: String =
+    """
+    query FavoriteAuthors {
+      favoriteAuthors {
+        __typename
+        picture
+        name
+        booksCount
+      }
+    }
+    """
+
+  public let operationName: String = "FavoriteAuthors"
+
+  public init() {
+  }
+
+  public struct Data: GraphQLSelectionSet {
+    public static let possibleTypes: [String] = ["Query"]
+
+    public static var selections: [GraphQLSelection] {
+      return [
+        GraphQLField("favoriteAuthors", type: .nonNull(.list(.nonNull(.object(FavoriteAuthor.selections))))),
+      ]
+    }
+
+    public private(set) var resultMap: ResultMap
+
+    public init(unsafeResultMap: ResultMap) {
+      self.resultMap = unsafeResultMap
+    }
+
+    public init(favoriteAuthors: [FavoriteAuthor]) {
+      self.init(unsafeResultMap: ["__typename": "Query", "favoriteAuthors": favoriteAuthors.map { (value: FavoriteAuthor) -> ResultMap in value.resultMap }])
+    }
+
+    public var favoriteAuthors: [FavoriteAuthor] {
+      get {
+        return (resultMap["favoriteAuthors"] as! [ResultMap]).map { (value: ResultMap) -> FavoriteAuthor in FavoriteAuthor(unsafeResultMap: value) }
+      }
+      set {
+        resultMap.updateValue(newValue.map { (value: FavoriteAuthor) -> ResultMap in value.resultMap }, forKey: "favoriteAuthors")
+      }
+    }
+
+    public struct FavoriteAuthor: GraphQLSelectionSet {
+      public static let possibleTypes: [String] = ["Author"]
+
+      public static var selections: [GraphQLSelection] {
+        return [
+          GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
+          GraphQLField("picture", type: .nonNull(.scalar(String.self))),
+          GraphQLField("name", type: .nonNull(.scalar(String.self))),
+          GraphQLField("booksCount", type: .nonNull(.scalar(Int.self))),
+        ]
+      }
+
+      public private(set) var resultMap: ResultMap
+
+      public init(unsafeResultMap: ResultMap) {
+        self.resultMap = unsafeResultMap
+      }
+
+      public init(picture: String, name: String, booksCount: Int) {
+        self.init(unsafeResultMap: ["__typename": "Author", "picture": picture, "name": name, "booksCount": booksCount])
+      }
+
+      public var __typename: String {
+        get {
+          return resultMap["__typename"]! as! String
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "__typename")
+        }
+      }
+
+      public var picture: String {
+        get {
+          return resultMap["picture"]! as! String
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "picture")
+        }
+      }
+
+      public var name: String {
+        get {
+          return resultMap["name"]! as! String
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "name")
+        }
+      }
+
+      public var booksCount: Int {
+        get {
+          return resultMap["booksCount"]! as! Int
+        }
+        set {
+          resultMap.updateValue(newValue, forKey: "booksCount")
+        }
+      }
+    }
+  }
+}
+
+public final class FavoriteBookQuery: GraphQLQuery {
+  /// The raw GraphQL definition of this operation.
+  public let operationDefinition: String =
+    """
+    query FavoriteBook($id: ID!) {
       book(id: $id) {
         __typename
         cover
@@ -22,7 +280,7 @@ public final class DetailsViewQuery: GraphQLQuery {
     }
     """
 
-  public let operationName: String = "DetailsView"
+  public let operationName: String = "FavoriteBook"
 
   public var id: GraphQLID
 
@@ -172,12 +430,11 @@ public final class DetailsViewQuery: GraphQLQuery {
   }
 }
 
-public final class HomeViewQuery: GraphQLQuery {
+public final class FavoriteBooksQuery: GraphQLQuery {
   /// The raw GraphQL definition of this operation.
   public let operationDefinition: String =
     """
-    query HomeView {
-      userPicture
+    query FavoriteBooks {
       favoriteBooks {
         __typename
         id
@@ -188,25 +445,10 @@ public final class HomeViewQuery: GraphQLQuery {
           name
         }
       }
-      favoriteAuthors {
-        __typename
-        picture
-        name
-        booksCount
-      }
-      allBooks {
-        __typename
-        cover
-        name
-        author {
-          __typename
-          name
-        }
-      }
     }
     """
 
-  public let operationName: String = "HomeView"
+  public let operationName: String = "FavoriteBooks"
 
   public init() {
   }
@@ -216,10 +458,7 @@ public final class HomeViewQuery: GraphQLQuery {
 
     public static var selections: [GraphQLSelection] {
       return [
-        GraphQLField("userPicture", type: .nonNull(.scalar(String.self))),
         GraphQLField("favoriteBooks", type: .nonNull(.list(.nonNull(.object(FavoriteBook.selections))))),
-        GraphQLField("favoriteAuthors", type: .nonNull(.list(.nonNull(.object(FavoriteAuthor.selections))))),
-        GraphQLField("allBooks", type: .nonNull(.list(.nonNull(.object(AllBook.selections))))),
       ]
     }
 
@@ -229,17 +468,8 @@ public final class HomeViewQuery: GraphQLQuery {
       self.resultMap = unsafeResultMap
     }
 
-    public init(userPicture: String, favoriteBooks: [FavoriteBook], favoriteAuthors: [FavoriteAuthor], allBooks: [AllBook]) {
-      self.init(unsafeResultMap: ["__typename": "Query", "userPicture": userPicture, "favoriteBooks": favoriteBooks.map { (value: FavoriteBook) -> ResultMap in value.resultMap }, "favoriteAuthors": favoriteAuthors.map { (value: FavoriteAuthor) -> ResultMap in value.resultMap }, "allBooks": allBooks.map { (value: AllBook) -> ResultMap in value.resultMap }])
-    }
-
-    public var userPicture: String {
-      get {
-        return resultMap["userPicture"]! as! String
-      }
-      set {
-        resultMap.updateValue(newValue, forKey: "userPicture")
-      }
+    public init(favoriteBooks: [FavoriteBook]) {
+      self.init(unsafeResultMap: ["__typename": "Query", "favoriteBooks": favoriteBooks.map { (value: FavoriteBook) -> ResultMap in value.resultMap }])
     }
 
     public var favoriteBooks: [FavoriteBook] {
@@ -248,24 +478,6 @@ public final class HomeViewQuery: GraphQLQuery {
       }
       set {
         resultMap.updateValue(newValue.map { (value: FavoriteBook) -> ResultMap in value.resultMap }, forKey: "favoriteBooks")
-      }
-    }
-
-    public var favoriteAuthors: [FavoriteAuthor] {
-      get {
-        return (resultMap["favoriteAuthors"] as! [ResultMap]).map { (value: ResultMap) -> FavoriteAuthor in FavoriteAuthor(unsafeResultMap: value) }
-      }
-      set {
-        resultMap.updateValue(newValue.map { (value: FavoriteAuthor) -> ResultMap in value.resultMap }, forKey: "favoriteAuthors")
-      }
-    }
-
-    public var allBooks: [AllBook] {
-      get {
-        return (resultMap["allBooks"] as! [ResultMap]).map { (value: ResultMap) -> AllBook in AllBook(unsafeResultMap: value) }
-      }
-      set {
-        resultMap.updateValue(newValue.map { (value: AllBook) -> ResultMap in value.resultMap }, forKey: "allBooks")
       }
     }
 
@@ -376,161 +588,48 @@ public final class HomeViewQuery: GraphQLQuery {
         }
       }
     }
+  }
+}
 
-    public struct FavoriteAuthor: GraphQLSelectionSet {
-      public static let possibleTypes: [String] = ["Author"]
+public final class UserPictureQuery: GraphQLQuery {
+  /// The raw GraphQL definition of this operation.
+  public let operationDefinition: String =
+    """
+    query UserPicture {
+      userPicture
+    }
+    """
 
-      public static var selections: [GraphQLSelection] {
-        return [
-          GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
-          GraphQLField("picture", type: .nonNull(.scalar(String.self))),
-          GraphQLField("name", type: .nonNull(.scalar(String.self))),
-          GraphQLField("booksCount", type: .nonNull(.scalar(Int.self))),
-        ]
-      }
+  public let operationName: String = "UserPicture"
 
-      public private(set) var resultMap: ResultMap
+  public init() {
+  }
 
-      public init(unsafeResultMap: ResultMap) {
-        self.resultMap = unsafeResultMap
-      }
+  public struct Data: GraphQLSelectionSet {
+    public static let possibleTypes: [String] = ["Query"]
 
-      public init(picture: String, name: String, booksCount: Int) {
-        self.init(unsafeResultMap: ["__typename": "Author", "picture": picture, "name": name, "booksCount": booksCount])
-      }
-
-      public var __typename: String {
-        get {
-          return resultMap["__typename"]! as! String
-        }
-        set {
-          resultMap.updateValue(newValue, forKey: "__typename")
-        }
-      }
-
-      public var picture: String {
-        get {
-          return resultMap["picture"]! as! String
-        }
-        set {
-          resultMap.updateValue(newValue, forKey: "picture")
-        }
-      }
-
-      public var name: String {
-        get {
-          return resultMap["name"]! as! String
-        }
-        set {
-          resultMap.updateValue(newValue, forKey: "name")
-        }
-      }
-
-      public var booksCount: Int {
-        get {
-          return resultMap["booksCount"]! as! Int
-        }
-        set {
-          resultMap.updateValue(newValue, forKey: "booksCount")
-        }
-      }
+    public static var selections: [GraphQLSelection] {
+      return [
+        GraphQLField("userPicture", type: .nonNull(.scalar(String.self))),
+      ]
     }
 
-    public struct AllBook: GraphQLSelectionSet {
-      public static let possibleTypes: [String] = ["Book"]
+    public private(set) var resultMap: ResultMap
 
-      public static var selections: [GraphQLSelection] {
-        return [
-          GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
-          GraphQLField("cover", type: .nonNull(.scalar(String.self))),
-          GraphQLField("name", type: .nonNull(.scalar(String.self))),
-          GraphQLField("author", type: .nonNull(.object(Author.selections))),
-        ]
+    public init(unsafeResultMap: ResultMap) {
+      self.resultMap = unsafeResultMap
+    }
+
+    public init(userPicture: String) {
+      self.init(unsafeResultMap: ["__typename": "Query", "userPicture": userPicture])
+    }
+
+    public var userPicture: String {
+      get {
+        return resultMap["userPicture"]! as! String
       }
-
-      public private(set) var resultMap: ResultMap
-
-      public init(unsafeResultMap: ResultMap) {
-        self.resultMap = unsafeResultMap
-      }
-
-      public init(cover: String, name: String, author: Author) {
-        self.init(unsafeResultMap: ["__typename": "Book", "cover": cover, "name": name, "author": author.resultMap])
-      }
-
-      public var __typename: String {
-        get {
-          return resultMap["__typename"]! as! String
-        }
-        set {
-          resultMap.updateValue(newValue, forKey: "__typename")
-        }
-      }
-
-      public var cover: String {
-        get {
-          return resultMap["cover"]! as! String
-        }
-        set {
-          resultMap.updateValue(newValue, forKey: "cover")
-        }
-      }
-
-      public var name: String {
-        get {
-          return resultMap["name"]! as! String
-        }
-        set {
-          resultMap.updateValue(newValue, forKey: "name")
-        }
-      }
-
-      public var author: Author {
-        get {
-          return Author(unsafeResultMap: resultMap["author"]! as! ResultMap)
-        }
-        set {
-          resultMap.updateValue(newValue.resultMap, forKey: "author")
-        }
-      }
-
-      public struct Author: GraphQLSelectionSet {
-        public static let possibleTypes: [String] = ["Author"]
-
-        public static var selections: [GraphQLSelection] {
-          return [
-            GraphQLField("__typename", type: .nonNull(.scalar(String.self))),
-            GraphQLField("name", type: .nonNull(.scalar(String.self))),
-          ]
-        }
-
-        public private(set) var resultMap: ResultMap
-
-        public init(unsafeResultMap: ResultMap) {
-          self.resultMap = unsafeResultMap
-        }
-
-        public init(name: String) {
-          self.init(unsafeResultMap: ["__typename": "Author", "name": name])
-        }
-
-        public var __typename: String {
-          get {
-            return resultMap["__typename"]! as! String
-          }
-          set {
-            resultMap.updateValue(newValue, forKey: "__typename")
-          }
-        }
-
-        public var name: String {
-          get {
-            return resultMap["name"]! as! String
-          }
-          set {
-            resultMap.updateValue(newValue, forKey: "name")
-          }
-        }
+      set {
+        resultMap.updateValue(newValue, forKey: "userPicture")
       }
     }
   }
--- SSBook/ViewController.swift
+++ SSBook/ViewController.swift
@@ -12,14 +12,6 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-//    ApolloNetwork.shared.apollo.fetch(query: DetailsViewQuery(id: "1")) { result in
-//      switch result {
-//      case .success(let detailsBook):
-//        print(detailsBook.data?.book)
-//      case .failure(let error):
-//        print(error.localizedDescription)
-//      }
-//    }
   }
 }
 
