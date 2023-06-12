ccbc547977cb396ebd1639adf86af8a40584f7c4
Victor <victor.a.vieira@hotmail.com>
2023-06-12 16:05:05 -0400

    Alterado nomes e a ordem da func.

--- SSBookUITests/SSBookUITests.swift
+++ SSBookUITests/SSBookUITests.swift
@@ -11,10 +11,12 @@ final class SSBookUITests: XCTestCase {
   
   func testFavoriteBookCellPressedAndReturn() {
     let app = XCUIApplication()
-    app.launch()
     let cellCleanCode = app.scrollViews.otherElements.collectionViews.cells.otherElements.containing(.staticText, identifier:"Clean Code: A Handbook of Agile Software Craftsmanship").element
+    let navigationBackButton = app.navigationBars["Início"].buttons["arrow.backward"]
+    
+    app.launch()
     cellCleanCode.tap()
-    app.navigationBars["Início"].buttons["arrow.backward"].tap()
+    navigationBackButton.tap()
   }
   
   func testScrollFavoriteBooks() {
@@ -22,13 +24,20 @@ final class SSBookUITests: XCTestCase {
     app.launch()
     
     let elementsFavoriteBooks = XCUIApplication().scrollViews.otherElements
-    elementsFavoriteBooks/*@START_MENU_TOKEN@*/.collectionViews.containing(.other, identifier:"Vertical scroll bar, 4 pages").element/*[[".collectionViews.containing(.other, identifier:\"Horizontal scroll bar, 4 pages\").element",".collectionViews.containing(.other, identifier:\"Vertical scroll bar, 4 pages\").element"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.swipeLeft()
+    let firstsElementsFavoriteBooks = XCUIApplication().scrollViews.otherElements/*@START_MENU_TOKEN@*/.collectionViews.containing(.other, identifier:"Vertical scroll bar, 4 pages").element/*[[".collectionViews.containing(.other, identifier:\"Horizontal scroll bar, 4 pages\").element",".collectionViews.containing(.other, identifier:\"Vertical scroll bar, 4 pages\").element"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
     
     let favoriteBooksCollectionViews = elementsFavoriteBooks.collectionViews
     let cellsFavoriteBooks = favoriteBooksCollectionViews.cells
-    cellsFavoriteBooks.otherElements.containing(.staticText, identifier:"The Hobbit").element.swipeLeft()
-    cellsFavoriteBooks.otherElements.containing(.staticText, identifier:"The Return").element.swipeLeft()
-    cellsFavoriteBooks.otherElements.containing(.staticText, identifier:"O Evangelho Segundo um Humorista").element/*@START_MENU_TOKEN@*/.swipeRight()/*[[".swipeDown()",".swipeRight()"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-    favoriteBooksCollectionViews/*@START_MENU_TOKEN@*/.staticTexts["Lonely Planet Chile & Easter Island"]/*[[".cells.staticTexts[\"Lonely Planet Chile & Easter Island\"]",".staticTexts[\"Lonely Planet Chile & Easter Island\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.swipeRight()
+    
+    let cellHobbit = cellsFavoriteBooks.otherElements.containing(.staticText, identifier:"The Hobbit").element
+    let cellReturn = cellsFavoriteBooks.otherElements.containing(.staticText, identifier:"The Return").element
+    let cellHumorista = cellsFavoriteBooks.otherElements.containing(.staticText, identifier:"O Evangelho Segundo um Humorista").element
+    let cellLonely = cellsFavoriteBooks.otherElements.containing(.staticText, identifier:"Lonely Planet Chile & Easter Island").element
+    
+    firstsElementsFavoriteBooks.swipeLeft()
+    cellHobbit.swipeLeft()
+    cellReturn.swipeLeft()
+    cellHumorista.swipeRight()
+    cellLonely.swipeRight()
   }
 }
